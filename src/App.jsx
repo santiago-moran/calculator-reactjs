342dcad0126549a056077ec7805ca8d86e4607a8
Santiago <santim366@gmail.com>
2024-06-06 15:44:08 -0300

    importing font awesome icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import '../styles.css'
 import { Button } from './components/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 export function App () {
     const buttons = Array(18).fill(null)
     const CONTENT = {
         0: 'C',
         1: '/',
-        2: 'X',
-        3: '+',
-        7: '-',
+        2: <FontAwesomeIcon icon={faXmark}/>,
+        3: <FontAwesomeIcon icon={faPlus}/>,
+        7: <FontAwesomeIcon icon={faMinus}/>,
         11: '.',
         15: <FontAwesomeIcon icon={faDeleteLeft}/>,
         17: '='
@@ -18,7 +21,6 @@ export function App () {
     let number = 1;
 
     return (
-        <>
         <section className='calculator'>
             <div className='output'></div>
             <section className='buttons'>
@@ -32,6 +34,5 @@ export function App () {
                 })}
             </section>
         </section>
-        </>
     )
 }
