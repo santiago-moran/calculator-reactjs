48feb9990892417fc4c077a0cbce5c660bfabec8
Santiago <santim366@gmail.com>
2024-06-06 12:47:11 -0300

    generating buttons content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import '../styles.css'
 import { Button } from './components/Button'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
 
 export function App () {
-    const buttons = Array(20).fill(null)
+    const buttons = Array(18).fill(null)
+    const CONTENT = {
+        0: 'C',
+        1: '/',
+        2: 'X',
+        3: '+',
+        7: '-',
+        11: '.',
+        15: <FontAwesomeIcon icon={faDeleteLeft}/>,
+        17: '='
+    }
+    let number = 1;
 
     return (
         <>
@@ -12,7 +25,9 @@ export function App () {
                 {buttons.map((_, index) => {
                     return (
                         <Button 
-                        key={index}/>
+                        key={index}
+                        content={CONTENT[index] ? CONTENT[index] : (number == 10 ? number = 0 : number++)}
+                        />
                     )
                 })}
             </section>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import '../../styles.css'
 
-export function Button () {
+export function Button ({content}) {
     return (
-        <div className='button'></div>
+        <div className={isNaN(parseInt(content)) && content == "=" ? "button symbol equal" : (isNaN(parseInt(content)) ? "button symbol" : "button")}>{content}</div>
     )
 }
